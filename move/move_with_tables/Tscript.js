33367399525a021428db5a8761f82a77df971479
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-07-11 17:53:21 -0400

    GOT IT checking to see if a bigger box is being put on a smaller box.

--- move/move_with_tables/Tscript.js
+++ move/move_with_tables/Tscript.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	var drag = $('.draggable');
 	var drop = $('.droppable');
 	var currentDrop;
+	var currentIdNum;
 // Identify draggable objects, return them back to where they were if
 // dropped in an illiegal place.
 	drop.droppable({
@@ -10,16 +11,39 @@ $(document).ready(function(){
 		var dropId = ui.draggable.attr("id");
 		// ui.draggable.addClass( 'dropped' );
 		// ui.draggable.data( 'droppedin', $(this));
-		$(this).addClass(dropId)
-		// $(this).droppable( 'disable' );
+		currentIdNum = dropId.charAt(1);
+		$(this).attr('id', dropId.charAt(1));
 		currentDrop = $(this).attr('id');
+		$('#' + dropId).addClass(currentDrop);
+		if($(this).hasClass('col1')){
+			console.log('col1');
+			var idLast = $('.col1').last().attr('id');
+			var dropingId = dropId.charAt(1);
+			console.log('last id', idLast);
+			console.log('dopping id', dropingId)
+			if (parseInt(idLast) < parseInt(dropingId)) {
+					console.log('too big');
+				}
+				};
+		// $(this).droppable( 'disable' );
+		// if($(this).parent().attr('id') == 'tr1') {
+		//   console.log('we are in the top row');
+		//   console.log($('.col1'));
+		//   // row.find('td').each(function(index) {
+		//   //   console.log(index.attr('id'));
+		//   //   });
+		// } else {
+		//   console.log('not in top row');
+		//   var row = $(this).parent().siblings();
+		//   console.log($('.col2'));
+		// }
 		return currentDrop;
 	}
 	});
 	drag.draggable({
 		start: function(event, ui){
 			var dragId = $(this).attr("id");
-			$('#' + currentDrop).removeClass(dragId);
+			// $('#' + currentIdNum).removeAttr('id');
 		},
 		revert: function(event, ui) {
 		var offset = $(this).offset();
@@ -32,10 +56,9 @@ $(document).ready(function(){
 		snap: '.droppable',
 		snapMode: 'inner',
 		drag: function(event, ui) {
-			if($(this).data('droppedin')){
 				// $(this).data('droppedin').droppable('enable')
 				// $(this).data('droppedin', null);
 				// $(this).removeClass('dropped')
-			}},
+			},
 				});
 });
